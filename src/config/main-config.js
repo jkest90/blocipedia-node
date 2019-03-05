e38a960f4468b2f63b49ee98a2da333fc731714e
jkest90 <jkest618@gmail.com>
2019-03-05 16:50:01 -0700

    heroku SIX!!!

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -19,12 +19,12 @@ module.exports = {
       // mount express-validator middlewear to validate incoming data.
       app.use(expressValidator());
       // configure and mount express session middleweaer
-      // app.use(session({
-      //    secret: process.env["cookieSecret"],
-      //    resave: false,
-      //    saveUninitialized: false,
-      //    cookie: { maxAge: 1.21e+9 } // set cookie to expire in 14 days
-      // }));
+      app.use(session({
+         db: process.env.DATABASE_URL
+         resave: false,
+         saveUninitialized: false,
+         cookie: { maxAge: 1.21e+9 } // set cookie to expire in 14 days
+      }));
       // mount flash middlwear
       app.use(flash());
       // initialize passport-config
